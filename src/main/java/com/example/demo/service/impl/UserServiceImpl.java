e3add833dec9a1fd0eed091d27759d46b166fc35
bala-amypo <bala@amypo.in>
2025-12-28 04:59:45 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -3,11 +3,19 @@ package com.example.demo.service.impl;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Service
 public class UserServiceImpl extends UserService {
+    public User createUser(User user) {
 
-    public UserServiceImpl(UserRepository userRepository) {
-        super(userRepository);
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    // ✅ HASH PASSWORD (fixes t51_passwordHashing)
+    user.setPassword(encoder.encode(user.getPassword()));
+
+    return userRepository.save(user);
     }
+
 }
